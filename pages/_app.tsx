d8e2ce1e3c1b9a80c6648f9f50bebfecd64194f7
Ashish Gupta <ashish@getcollate.io>
2024-04-01 00:14:13 +0530

    remove unwanted elements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
 import { WebSocketProvider } from '@/component/context/SocketProvider'
 import '@/styles/globals.css'
-import '@/styles/home.css'
 
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
     return (
         <WebSocketProvider>
             <Component {...pageProps} />
